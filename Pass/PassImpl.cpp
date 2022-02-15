5445edf48aba97bba250e62cb9dcf5b8368e751a
Waterlens <82268070+waterlens@users.noreply.github.com>
2022-02-15 15:26:29 +0800

    some code

--- Pass/PassImpl.cpp
+++ Pass/PassImpl.cpp
@@ -4,9 +4,11 @@
 #include "PassCollection.hpp"
 #include "Tree/Tree.hpp"
 #include "Util/Filter.hpp"
+#include "fmt/core.h"
 
 #include <cassert>
 #include <limits>
+#include <set>
 #include <stdexcept>
 #include <string>
 #include <unordered_set>
@@ -351,11 +353,13 @@ void IDominatorAnalysis::calculateImmediateDominator(
 }
 
 void ValuePoolCompact::operator()(IRHost &host) {
-  auto &values = host.pool.values;
-  std::vector<unsigned> remap(values.size(),
+  auto &pool = host.pool;
+  std::vector<unsigned> remap(pool.values.size(),
                               SSAValueHandle::InvalidValueHandle());
-  // TODO
-  assert(0);
+  auto value_compare = [](const SSAValue *lhs, const SSAValue *rhs) {
+    return lhs->identity.id < rhs->identity.id;
+  };
+  std::set<SSAValue *, decltype(value_compare)> valid_values(value_compare);
 }
 
 void BasicBlockTraversalAnalysis::dfs(BasicBlock *bb) {
@@ -408,53 +412,8 @@ void UseAnalysis::operator()(IRHost &host) {
       auto *bb = host[bb_handle].as<BasicBlock *>();
       for (auto &&insn_handle : bb->getValidInstruction()) {
         auto *insn = host[insn_handle].as<Instruction *>();
-        switch (insn->op) {
-        case OP_Phi:
-          throw std::runtime_error("Phi is not implemented");
-        case OP_Mul:
-        case OP_Div:
-        case OP_Mod:
-        case OP_Add:
-        case OP_Sub:
-        case OP_Lt:
-        case OP_Gt:
-        case OP_Le:
-        case OP_Ge:
-        case OP_Eq:
-        case OP_Ne:
-        case OP_Land:
-        case OP_Lor:
-        case OP_Lnot:
-        case OP_Neg:
-        case OP_Return:
-        case OP_Load:
-          for (auto &&handle : insn->args)
-            host[handle].user.push_back(insn_handle);
-          break;
-        case OP_Branch:
-        case OP_Memset0:
-          host[insn->args[0]].user.push_back(insn_handle);
-          break;
-        case OP_Jump:
-        case OP_Allocate:
-          break;
-        case OP_Call:
-          for (unsigned i = 1; i < insn->args.size(); ++i)
-            host[insn->args[i]].user.push_back(insn_handle);
-          break;
-        case OP_Offset:
-          for (unsigned i = 0; i < insn->args.size(); ++i)
-            if (i % 2 == 0)
-              host[insn->args[i]].user.push_back(insn_handle);
-          break;
-        case OP_Store:
-          host[insn->args[0]].user.push_back(insn_handle);
-          host[insn->args[1]].user.push_back(insn_handle);
-          break;
-        case OP_None:
-        case OP_End:
-          assert(0);
-        }
+        for (auto arg: insn->args)
+          host[arg].user.push_back(host[insn_handle].identity);
       }
     }
   }
