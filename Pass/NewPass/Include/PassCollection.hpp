f41e3bcc1b90ed1f797fd3f74390888b038e8a1c
Waterlens <82268070+waterlens@users.noreply.github.com>
2022-02-22 17:09:35 +0800

    separate declarations and definitions

--- Pass/NewPass/Include/PassCollection.hpp
+++ Pass/NewPass/Include/PassCollection.hpp
@@ -2,6 +2,8 @@
 
 namespace YIR {
 class Tree2SSA;
+class IRDump;
 }
 
 #include "Tree2SSA.hpp"
+#include "Dump.hpp"
---
+++ Pass/NewPass/Source/Dump.cpp
@@ -0,0 +1,116 @@
+#include "../Include/Dump.hpp"
+
+namespace YIR {\
+std::string IRDump::dumpType(const Type &ty) {
+  std::string buffer;
+  switch (ty.primitive_type) {
+  case Type::PrimitiveType::Void:
+    buffer += "void";
+    break;
+  case Type::PrimitiveType::Integer:
+    buffer += fmt::format("i{}", ty.width);
+    break;
+  }
+  buffer += std::string(ty.pointer, '*');
+  return buffer;
+}
+
+std::string IRDump::dumpUser(Value *value) {
+  return fmt::format(
+    "// {}",
+    join(
+      value->getImmutableEdges(), ListIterator<UseEdge>(),
+      [](auto edge) { return fmt::format("%{}", edge.to->getIdentity()); },
+      " "));
+}
+
+std::string
+IRDump::dumpFunctionParameterList(const std::vector<Argument *> &param) {
+  return join(
+    param.begin(), param.end(),
+    [](auto arg) {
+      return fmt::format("#{}: {} {}", arg->name, dumpType(arg->type),
+                         dumpUser(arg));
+    },
+    ", ");
+}
+
+std::string IRDump::dumpInstructionInput(Value *value) {
+  if (value == nullptr)
+    return "<nullptr>";
+  if (auto *ci = value->as<ConstantInteger *>())
+    return fmt::format("{}", ci->value);
+  if (auto *insn = value->as<Instruction *>())
+    return fmt::format("{} %{}", dumpType(insn->type), insn->getIdentity());
+  if (auto *arg = value->as<Argument *>())
+    return fmt::format("{} #{}", dumpType(arg->type), arg->name);
+  if (auto *gv = value->as<GlobalVariable *>())
+    return fmt::format("{} @{}.addr", dumpType(gv->type), gv->name);
+  if (auto *bb = value->as<BasicBlock *>())
+    return fmt::format("label L{}", bb->getIdentity());
+  if (auto *f = value->as<Function *>())
+    return fmt::format("fn {} {}", dumpType(f->return_type), f->name);
+  throw std::runtime_error("can not dump this SSAValue");
+}
+
+void IRDump::dumpAllGlobalVariable(IRHost &host) {
+  std::for_each(host.getModule()->global.begin(),
+                host.getModule()->global.end(), [&](GlobalVariable *gv) {
+                  buffer +=
+                    fmt::format("@{}.addr : {}, region size {} {}\n", gv->name,
+                                dumpType(gv->type), gv->capacity, dumpUser(gv));
+                });
+  buffer += "\n";
+}
+
+void IRDump::dumpInstruction(Instruction *insn) {
+  buffer += fmt::format("    {} %{} <- {} ", dumpType(insn->type),
+                        insn->getIdentity(), op_name[insn->op]);
+  buffer += join(
+    insn->getInput().begin(), insn->getInput().end(),
+    [this](auto v) { return dumpInstructionInput(v.from); }, ", ");
+  buffer += fmt::format(" {}\n", dumpUser(insn));
+}
+
+void IRDump::dumpBasicBlock(BasicBlock *bb) {
+  buffer += fmt::format("L{}:\n", bb->getIdentity());
+  std::for_each(bb->begin(), bb->end(),
+                [&](auto insn) { dumpInstruction(&insn); });
+}
+
+void IRDump::dumpFunction(Function *func) {
+  buffer += fmt::format("fn {} ({}) -> {}", func->name,
+                        dumpFunctionParameterList(func->arg),
+                        dumpType(func->return_type));
+  if (func->external)
+    buffer += ";";
+  else {
+    buffer += " {\n";
+    std::for_each(func->block.begin(), func->block.end(),
+                  [&](auto bb) { dumpBasicBlock(&bb); });
+    buffer += "}";
+  }
+  buffer += "\n\n";
+}
+
+void IRDump::assignIdentity(IRHost &host) {
+  std::for_each(
+    host.getModule()->func.begin(), host.getModule()->func.end(), [&](auto &f) {
+      std::for_each(f->block.begin(), f->block.end(), [&](auto &bb) {
+        bb.getIdentity() = ++id;
+        std::for_each(bb.begin(), bb.end(),
+                      [&](auto &insn) { insn.getIdentity() = ++id; });
+      });
+    });
+}
+
+void IRDump::dumpIRText(IRHost &host) {
+  assignIdentity(host);
+  dumpAllGlobalVariable(host);
+  std::for_each(host.getModule()->func.begin(), host.getModule()->func.end(),
+                [&](auto f) { dumpFunction(f); });
+  static int ir_count = 0;
+  auto out = fmt::output_file(fmt::format("dump.new-ir.{}.txt", ir_count++));
+  out.print("{}", buffer);
+}
+} // namespace YIR
