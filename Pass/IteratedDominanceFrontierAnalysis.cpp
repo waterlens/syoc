6bf20c6801e12d0a01696dc612d3b7cb1370e548
Waterlens <waterlens@waterlens.moe>
2022-03-02 21:34:39 +0800

    update code

--- Pass/IteratedDominanceFrontierAnalysis.cpp
+++ Pass/IteratedDominanceFrontierAnalysis.cpp
@@ -10,10 +10,13 @@ void IteratedDominanceFrontierAnalysis::computeDFLocal(
   for (auto &y : x->getSuccessor()) {
     if (!idom.contains(y.to))
       throw std::runtime_error("Successor has no dominator");
-    if (idom.at(y.to) != x)
+    if (idom.at(y.to) != x) {
       dominance_frontier.insert(x, y.to);
+      dominance_frontier_set.insert(y.to);
+    }
   }
 }
+
 void IteratedDominanceFrontierAnalysis::computeDFUp(
   const IDominatorAnalysis &ida, BasicBlock *x, BasicBlock *z) {
   auto [idom, _] = ida.getIDominatorMap();
@@ -21,14 +24,46 @@ void IteratedDominanceFrontierAnalysis::computeDFUp(
   for (auto y_iter = range.first; y_iter != range.second; ++y_iter) {
     if (!idom.contains(y_iter->second) || !idom.contains(z))
       throw std::runtime_error("y has no dominator");
-    if (idom.at(y_iter->second) != x && idom.at(z) == x /* redundancy */)
+    if (idom.at(y_iter->second) != x && idom.at(z) == x /* redundancy */) {
       dominance_frontier.insert(x, y_iter->second);
+      dominance_frontier_set.insert(y_iter->second);
+    }
   }
 }
+
 void IteratedDominanceFrontierAnalysis::computeDominanceFrontier(
   const IDominatorAnalysis &ida, BasicBlock *x) {
   auto po = ida.dominanceTreeTraversal<true, false>(x);
   for (auto *bb : po) {
+    computeDFLocal(ida, bb);
+    auto idominated = ida.findAllDominated(bb);
+    for (auto *z : idominated) computeDFUp(ida, bb, z);
   }
 }
+
+void IteratedDominanceFrontierAnalysis::computeDominanceFrontierSet(
+  const IDominatorAnalysis &ida, const std::unordered_set<BasicBlock *> &set) {
+  for (auto *bb : set) computeDominanceFrontier(ida, bb);
+}
+
+void IteratedDominanceFrontierAnalysis::computeIteratedDominanceFrontierSet(
+  const IDominatorAnalysis &ida, std::unordered_set<BasicBlock *> &set) {
+  dominance_frontier_set.merge(set);
+
+  bool changed = true;
+  auto dfp_size = dominance_frontier_set.size();
+
+  while (changed) {
+    changed = false;
+
+    computeDominanceFrontierSet(ida, dominance_frontier_set);
+    auto dfp_size_new = dominance_frontier_set.size();
+
+    if (dfp_size != dfp_size_new) {
+      changed = true;
+      dfp_size = dfp_size_new;
+    }
+  }  
+}
+
 } // namespace SyOC
--- Pass/IteratedDominanceFrontierAnalysis.hpp
+++ Pass/IteratedDominanceFrontierAnalysis.hpp
@@ -11,10 +11,17 @@ namespace SyOC {
 class IteratedDominanceFrontierAnalysis {
 private:
   std::unordered_multimap<BasicBlock *, BasicBlock *> dominance_frontier;
+  std::unordered_set<BasicBlock *> dominance_frontier_set;
   void computeDFLocal(const IDominatorAnalysis &ida, BasicBlock *x);
   void computeDFUp(const IDominatorAnalysis &ida, BasicBlock *x, BasicBlock *z);
   void computeDominanceFrontier(const IDominatorAnalysis &ida, BasicBlock *x);
-
+  void computeDominanceFrontierSet(const IDominatorAnalysis &ida,
+                                   const std::unordered_set<BasicBlock *> &set);
+  void
+  computeIteratedDominanceFrontierSet(const IDominatorAnalysis &ida,
+                                      std::unordered_set<BasicBlock *> &set);
+  
+  
 public:
   IteratedDominanceFrontierAnalysis() = default;
   [[nodiscard]] static std::string_view getName() {
