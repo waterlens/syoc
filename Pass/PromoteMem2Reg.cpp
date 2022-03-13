a3149cad9118f642be8f7fa11a36c50bb4bb2dc6
Waterlens <waterlens@waterlens.moe>
2022-03-13 15:55:19 +0800

    move impl position

--- Pass/PromoteMem2Reg.cpp
+++ Pass/PromoteMem2Reg.cpp
@@ -1,2 +1,123 @@
 #include "PromoteMem2Reg.hpp"
 
+namespace SyOC {
+
+void PromoteMem2Reg::insertPhiForAlloca(IteratedDominanceFrontierAnalysis &idfa,
+                                        Instruction &alloca) {
+  assert(isPromotable(alloca));
+  auto def_bb = findDefinitionBlock(alloca);
+  auto [_, idf] = idfa.getIDFSet(def_bb);
+  for (auto *bb : idf) {
+    auto *phi = Instruction::create(OP_Phi, PredefinedType::Int32);
+    phi->refParent() = bb;
+    bb->getInstruction().push_front(phi);
+    phiAllocaMap[phi] = &alloca;
+    allocaPhiMap[&alloca].push_back(phi);
+  }
+}
+
+void PromoteMem2Reg::renamePhiInSuccessorBasicBlock(BasicBlock &bb,
+                                                    BasicBlock &pred) {
+  for (auto &phi : bb.getInstruction()) {
+    if (phi.op != OP_Phi)
+      break;
+    if (!phiAllocaMap.contains(&phi))
+      throw std::runtime_error("phi is not associated with an allocation");
+    auto *alloc = phiAllocaMap.at(&phi);
+    if (containValue(&pred, alloc)) {
+      phi.addInput(&pred);
+      phi.addInput(getValue(&pred, alloc));
+    }
+  }
+}
+
+void PromoteMem2Reg::renamePhi(const IDominatorAnalysis &ida, BasicBlock &bb) {
+  for (auto insn_iter = bb.begin(); insn_iter != bb.end();) {
+    if (insn_iter->op == OP_Phi) {
+      if (!phiAllocaMap.contains(insn_iter.base()))
+        throw std::runtime_error("phi is not associated with an allocation");
+      auto *alloc = phiAllocaMap.at(insn_iter.base());
+      allocaLatest[&bb][alloc] = insn_iter.base();
+    } else if (insn_iter->op == OP_Load) {
+      auto *v = insn_iter->getInput()[0].from;
+      if (v->is<Instruction *>() &&
+          v->as<Instruction *>()->op == OP_Allocate) {
+        auto *alloc = v->as<Instruction *>();
+        Value *latest;
+        if (containValue(&bb, alloc))
+          latest = getValue(&bb, alloc);
+        else
+          throw std::runtime_error("undef here");
+        insn_iter->replaceAllUsesWith(latest);
+        insn_iter.release_and_increase(true);
+        continue;
+      }
+    } else if (insn_iter->op == OP_Store) {
+      auto *v = insn_iter->getInput()[0].from;
+      if (v->is<Instruction *>() &&
+          v->as<Instruction *>()->op == OP_Allocate) {
+        auto *alloc = v->as<Instruction *>();
+        auto *src = insn_iter->getInput()[1].from;
+        assert(alloc != nullptr && alloc->op == OP_Allocate);
+        allocaLatest[&bb][alloc] = src;
+        insn_iter.release_and_increase(true);
+        continue;
+      }
+    }
+    ++insn_iter;
+  }
+
+  auto [_, idom] = ida.getIDominatorMap();
+  auto range = idom.equal_range(&bb);
+  for (auto iter = range.first; iter != range.second; iter++) {
+    if (allocaLatest.contains(&bb))
+      allocaLatest[iter->second] = allocaLatest[&bb];
+  }
+
+  for (auto &e : bb.getSuccessor()) {
+    auto *succ = e.to;
+    renamePhiInSuccessorBasicBlock(*succ, bb);
+  }
+}
+
+void PromoteMem2Reg::promoteMem2Reg(const IDominatorAnalysis &ida,
+                                    IteratedDominanceFrontierAnalysis &idfa,
+                                    Function &func) {
+  if (func.refExternal())
+    return;
+  auto &alloca_bb = func.block.front();
+  for (auto &alloca_insn : alloca_bb) {
+    if (isPromotable(alloca_insn))
+      insertPhiForAlloca(idfa, alloca_insn);
+  }
+  auto bfs = bfsTraversal<false>(&func);
+  for (auto *bb : bfs) renamePhi(ida, *bb);
+  clearVisited(&func);
+}
+
+void PromoteMem2Reg::operator()(IRHost &host) {
+  IteratedDominanceFrontierAnalysis idfa;
+  IDominatorAnalysis ida;
+  idfa(host);
+  ida(host);
+
+  for (auto *func : host.getModule()->func) {
+    if (func->refExternal())
+      continue;
+    promoteMem2Reg(ida, idfa, *func);
+  }
+}
+
+Value *PromoteMem2Reg::getValue(BasicBlock *bb, Instruction *alloc) {
+  return allocaLatest.at(bb).at(alloc);
+}
+
+bool PromoteMem2Reg::containValue(BasicBlock *bb, Instruction *alloc) {
+  return allocaLatest.contains(bb) && allocaLatest.at(bb).contains(alloc);
+}
+
+bool PromoteMem2Reg::isPromotable(Instruction &alloca) {
+  return alloca.op == OP_Allocate &&
+         alloca.getInput()[0].from->as<SyOC::ConstantInteger *>()->value == 4;
+}
+} // namespace SyOC
--- Pass/PromoteMem2Reg.hpp
+++ Pass/PromoteMem2Reg.hpp
@@ -17,140 +17,27 @@
 
 namespace SyOC {
 class PromoteMem2Reg {
-  struct PairHash {
-    template <class T1, class T2>
-    std::size_t operator()(const std::pair<T1, T2> &p) const {
-      auto h1 = std::hash<T1>()(p.first);
-      auto h2 = std::hash<T2>()(p.second);
-      return h1 + 0x9e3779b9 + (h2 << 6) + (h2 >> 2);
-    }
-  };
   std::unordered_map<Instruction *, std::vector<Instruction *>> allocaPhiMap;
   std::unordered_map<Instruction *, Instruction *> phiAllocaMap;
   std::unordered_map<BasicBlock *, std::unordered_map<Instruction *, Value *>>
     allocaLatest;
   IDominatorAnalysis idom;
 
-  static bool isPromotable(Instruction &alloca) {
-    return alloca.op == OP_Allocate &&
-           alloca.getInput()[0].from->as<SyOC::ConstantInteger *>()->value == 4;
-  }
-
-  bool containValue(BasicBlock *bb, Instruction *alloc) {
-    return allocaLatest.contains(bb) && allocaLatest.at(bb).contains(alloc);
-  }
-
-  Value *getValue(BasicBlock *bb, Instruction *alloc) {
-    return allocaLatest.at(bb).at(alloc);
-  }
-
+  static bool isPromotable(Instruction &alloca);
+  bool containValue(BasicBlock *bb, Instruction *alloc);
+  Value *getValue(BasicBlock *bb, Instruction *alloc);
   void insertPhiForAlloca(IteratedDominanceFrontierAnalysis &idfa,
-                          Instruction &alloca) {
-    assert(isPromotable(alloca));
-    auto def_bb = findDefinitionBlock(alloca);
-    auto [_, idf] = idfa.getIDFSet(def_bb);
-    for (auto *bb : idf) {
-      auto *phi = Instruction::create(OP_Phi, PredefinedType::Int32);
-      phi->refParent() = bb;
-      bb->getInstruction().push_front(phi);
-      phiAllocaMap[phi] = &alloca;
-      allocaPhiMap[&alloca].push_back(phi);
-    }
-  }
-
-  void renamePhiInSuccessorBasicBlock(BasicBlock &bb, BasicBlock &pred) {
-    for (auto &phi : bb.getInstruction()) {
-      if (phi.op != OP_Phi)
-        break;
-      if (!phiAllocaMap.contains(&phi))
-        throw std::runtime_error("phi is not associated with an allocation");
-      auto *alloc = phiAllocaMap.at(&phi);
-      if (containValue(&pred, alloc)) {
-        phi.addInput(&pred);
-        phi.addInput(getValue(&pred, alloc));
-      }
-    }
-  }
-
-  void renamePhi(const IDominatorAnalysis &ida, BasicBlock &bb) {
-    for (auto insn_iter = bb.begin(); insn_iter != bb.end();) {
-      if (insn_iter->op == OP_Phi) {
-        if (!phiAllocaMap.contains(insn_iter.base()))
-          throw std::runtime_error("phi is not associated with an allocation");
-        auto *alloc = phiAllocaMap.at(insn_iter.base());
-        allocaLatest[&bb][alloc] = insn_iter.base();
-      } else if (insn_iter->op == OP_Load) {
-        auto *v = insn_iter->getInput()[0].from;
-        if (v->is<Instruction *>() &&
-            v->as<Instruction *>()->op == OP_Allocate) {
-          auto *alloc = v->as<Instruction *>();
-          Value *latest;
-          if (containValue(&bb, alloc))
-            latest = getValue(&bb, alloc);
-          else
-            throw std::runtime_error("undef here");
-          insn_iter->replaceAllUsesWith(latest);
-          insn_iter.release_and_increase(true);
-          continue;
-        }
-      } else if (insn_iter->op == OP_Store) {
-        auto *v = insn_iter->getInput()[0].from;
-        if (v->is<Instruction *>() &&
-            v->as<Instruction *>()->op == OP_Allocate) {
-          auto *alloc = v->as<Instruction *>();
-          auto *src = insn_iter->getInput()[1].from;
-          assert(alloc != nullptr && alloc->op == OP_Allocate);
-          allocaLatest[&bb][alloc] = src;
-          insn_iter.release_and_increase(true);
-          continue;
-        }
-      }
-      ++insn_iter;
-    }
-
-    auto [_, idom] = ida.getIDominatorMap();
-    auto range = idom.equal_range(&bb);
-    for (auto iter = range.first; iter != range.second; iter++) {
-      if (allocaLatest.contains(&bb))
-        allocaLatest[iter->second] = allocaLatest[&bb];
-    }
-
-    for (auto &e : bb.getSuccessor()) {
-      auto *succ = e.to;
-      renamePhiInSuccessorBasicBlock(*succ, bb);
-    }
-  }
-
+                          Instruction &alloca);
+  void renamePhiInSuccessorBasicBlock(BasicBlock &bb, BasicBlock &pred);
+  void renamePhi(const IDominatorAnalysis &ida, BasicBlock &bb);
   void promoteMem2Reg(const IDominatorAnalysis &ida,
-                      IteratedDominanceFrontierAnalysis &idfa, Function &func) {
-    if (func.refExternal())
-      return;
-    auto &alloca_bb = func.block.front();
-    for (auto &alloca_insn : alloca_bb) {
-      if (isPromotable(alloca_insn))
-        insertPhiForAlloca(idfa, alloca_insn);
-    }
-    auto bfs = bfsTraversal<false>(&func);
-    for (auto *bb : bfs) renamePhi(ida, *bb);
-    clearVisited(&func);
-  }
+                      IteratedDominanceFrontierAnalysis &idfa, Function &func);
 
 public:
   PromoteMem2Reg() = default;
   [[nodiscard]] static std::string_view getName() {
     return "Promote Memory to Register";
   }
-  void operator()(IRHost &host) {
-    IteratedDominanceFrontierAnalysis idfa;
-    IDominatorAnalysis ida;
-    idfa(host);
-    ida(host);
-
-    for (auto *func : host.getModule()->func) {
-      if (func->refExternal())
-        continue;
-      promoteMem2Reg(ida, idfa, *func);
-    }
-  }
+  void operator()(IRHost &host);
 };
 } // namespace SyOC
