7734955b9b8b6b1a6f80bd6cf569fcac4528f4a7
Waterlens <waterlens@waterlens.moe>
2022-03-26 11:24:45 +0800

    add some definition

--- IR/ASM.hpp
+++ IR/ASM.hpp
@@ -1,5 +1,48 @@
 #pragma once
 
-namespace SyOC {
+#include "IR/IR.hpp"
+#include "Util/List.hpp"
+#include <variant>
 
-}
+namespace SyOC::ARMv7a {
+
+struct Register {
+  unsigned id = 0;
+};
+
+struct RegisterList {
+  short ls = 0;
+};
+
+struct Shift {
+  enum class Type {
+#define ShiftTypeDefine(x, y) x,
+#include "Common/Common.def"
+#undef ShiftTypeDefine
+  } type;
+  int imm;
+  Register reg;
+};
+
+enum class Condition {
+#define ConditionTypeDefine(x, v) x = (v),
+#include "Common/Common.def"
+#undef ConditionTypeDefine
+};
+
+struct Address {
+  Register base;
+  std::variant<unsigned, Register, Shift> offset;
+};
+
+struct MBasicBlock;
+struct MInstruction;
+struct MFunction;
+struct MModule;
+
+struct MInstruction : public ListNode<MInstruction> {
+};
+
+struct MBasicBlock : public ListNode<MBasicBlock> {};
+
+} // namespace SyOC::ARMv7a
